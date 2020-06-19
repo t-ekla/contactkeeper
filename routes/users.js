9a7fe2a756242ca2682600ee68a23263d1d11640
Francesca Chater <francescachater@gmail.com>
2020-06-19 15:21:17 +0100

    bcrypt encrypts password

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
 const { check, validationResult } = require("express-validator/check");
 
 const User = require("../models/User");
@@ -17,13 +18,43 @@ router.post(
       "Please enter a password with six or more characters"
     ).isLength({ min: 6 }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
-    if( !errors.isEmpty() ) {
-        return res.status(400).json({ errors: errors.array() })
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('passed')
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      // Same as doing name: name, email: email etc
+      user = new User({
+        name,
+        email,
+        password,
+      });
+
+      // Encrypt password with bcrypt
+      const salt = await bcrypt.genSalt(10);
+
+      // Gives us hash version of the password which we are then assigning to user object
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send('User saved');
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server error')
+
+    }
   }
 );
 
