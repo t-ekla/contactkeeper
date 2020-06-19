354d9053e864ef9a84153db85c673060511dd142
Francesca Chater <francescachater@gmail.com>
2020-06-19 15:08:05 +0100

    Routes work with Postman

--- config/db.js
+++ config/db.js
@@ -2,18 +2,19 @@ const mongoose = require("mongoose");
 const config = require("config");
 const db = config.get("mongoURI");
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
-    })
-    .then(() => console.log("MongoDB Connected")) // going to give us a promise
-    .catch((error) => {
-      console.error(error.message);
-      process.exit(1);
     });
+
+    console.log("MongoDB Connected...");
+  } catch (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
